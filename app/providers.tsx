a4b85dc376ac210fa6284c9649568a5955ef7a2a
Gyanendra <gyanendramail04@gmail.com>
2025-12-29 04:38:29 -0500

    Fix stale session by enabling refetch in SessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,5 +7,12 @@ export default function Providers({
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      refetchOnWindowFocus={true}
+      refetchInterval={30}
+    >
+      {children}
+    </SessionProvider>
+  );
 }
